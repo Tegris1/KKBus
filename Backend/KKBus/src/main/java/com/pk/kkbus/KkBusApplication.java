784fe4128b10e96744cedca9c5510dbced961658
JakubJasek <jakub.jasek@student.pk.edu.pl>
2026-03-24 13:26:50 +0100

    Backend Initialization

---
+++ Backend/KKBus/src/main/java/com/pk/kkbus/KkBusApplication.java
@@ -0,0 +1,13 @@
+package com.pk.kkbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkBusApplication.class, args);
+    }
+
+}
---
+++ Backend/KKBus/src/test/java/com/pk/kkbus/KkBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.kkbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
